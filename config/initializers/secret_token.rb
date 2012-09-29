b4c6562fca17b922b8eeabca1a82063f2f4dd316
Amir Rubin <Amir.Rubin@acceleratedperception.com>
2012-09-29 10:57:17 -0400

    New Rails project wooo!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileVet::Application.config.secret_token = '21a1f1efe888b4e5f33ff9eeab1192e2c6b5e14bbfa7111f757f8bbfe4d1f6855b86cd51144687081bf5fc6f1a1007c123dd99b58fa23e16350ac3d02e59857d'

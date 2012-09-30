b6ee2567585d4db5685866638a25f35c9d89998d
Amir Rubin <Amir.Rubin@acceleratedperception.com>
2012-09-30 01:56:03 -0400

    Don't use 'or', use ||

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @visits = current_user.client_visits if current_user.is_client?
 
-    @appointments = current_user.provider_visits if current_user.is_technician? or current_user.is_vet?
-    @requests = current_user.outstanding_requests.each if current_user.is_technician? or current_user.is_vet?
+    @appointments = current_user.provider_visits if current_user.is_technician? || current_user.is_vet?
+    @requests = current_user.outstanding_requests if current_user.is_technician? || current_user.is_vet?
   end
 end

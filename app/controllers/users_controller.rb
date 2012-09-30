b68c230f097d8fb217d29fce779e7e70f560c0c7
Amir Rubin <Amir.Rubin@acceleratedperception.com>
2012-09-30 01:49:59 -0400

    Faking good names for models and rendering as collections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @visits = current_user.client_visits if current_user.is_client?
-    @visits = current_user.provider_visits if current_user.is_technician? or current_user.is_vet?
+
+    @appointments = current_user.provider_visits if current_user.is_technician? or current_user.is_vet?
+    @requests = current_user.outstanding_requests.each if current_user.is_technician? or current_user.is_vet?
   end
 end
